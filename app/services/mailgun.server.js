71458323f241a0bbc9e5ca7d23414fa77acd9254
Sameer <144911898+sameer266@users.noreply.github.com>
2026-06-17 09:19:50 +0545

    Update mailgun.server.js

--- app/services/mailgun.server.js
+++ app/services/mailgun.server.js
@@ -4,7 +4,7 @@ import FormData from "form-data";
 
 const MAILGUN_API_KEY = process.env.MAILGUN_API_KEY || "";
 const MAILGUN_DOMAIN = process.env.MAILGUN_DOMAIN || "";
-const MAILGUN_FROM_EMAIL = "dev@ellabache.com.au"
+const MAILGUN_FROM_EMAIL = "ellabache.com.au"
 const MAILGUN_FROM_NAME = "Ella Baché"
 
 
